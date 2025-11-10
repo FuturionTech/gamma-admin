d00bda0dc07e72b96403ebbdced59046bfac747d
acompaore <acompaore@futurion.tech>
2025-11-09 21:12:29 -0500

    fix: remove duplicate PageHeader from PartnersCreate

--- domains/partners/pages/PartnersCreate.vue
+++ domains/partners/pages/PartnersCreate.vue
@@ -3,9 +3,6 @@
     <div class="d-flex flex-column flex-column-fluid">
       <div id="kt_app_content" class="app-content flex-column-fluid">
         <div id="kt_app_content_container" class="app-container container-fluid">
-          <!-- Page Header -->
-          <PageHeader title="Create Partner" />
-
           <div class="card">
             <div class="card-body">
               <!-- Form -->
